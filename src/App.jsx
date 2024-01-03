304d8b97d5995043e8cef604e8ba8f994aff88ee
Getonto3 <getontomoses18@gmail.com>
2024-01-03 13:30:00 +0300

    Implementing the ShowList component for fetching from the API

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+
+import PostList from "./components/PostList";
+import ShowList from "./components/ShowList";
+
+const App = () => {
+  return (
+    <div>
+      < ShowList/>
+      < PostList />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ShowList.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+export default function ShowList() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/quickstart/");
+        const data = await response.json();
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getPosts();
+  }, []);
+
+  return (
+    <>
+      {" "}
+      <h2>SNIPPETS OF CODE</h2>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h4>{post.title}</h4>
+          <p>{post.code}</p>
+          <p>{post.linenos}</p>
+          <p>{post.language}</p>
+          <p>{post.style}</p>
+          <p>{post.owner}</p>
+        </div>
+      ))}
+    </>
+  );
+}
